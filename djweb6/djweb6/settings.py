7ccffc45f322ce0c23e2bd17c1b34dc15b5f1bd2
Patrick <pvd17@case.edu>
2017-05-02 00:55:46 -0400

    Changed settings to be able to hold mysql

--- djweb6/djweb6/settings.py
+++ djweb6/djweb6/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'djweb6.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pvd17$jurassic_park',
+        'USER': 'pvd17',
+        'PASSWORD': 'polar9697',
+        'HOST': 'pvd17.mysql.pythonanywhere-services.com',
+        'TEST': {
+          NAME: 'pvd17$jurassic_park',
     }
 }
 
