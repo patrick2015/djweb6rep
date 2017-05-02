27eee7741f4fb79baf522d6abb1d9d5bf8569b06
Patrick <pvd17@case.edu>
2017-05-02 02:21:53 -0400

    Added templates and altered views.py

--- djweb6/djapp1/views.py
+++ djweb6/djapp1/views.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import connection
 
 
 # Create your views here.
-
 def homepage(request):
     '''
     Context format:
@@ -18,7 +15,54 @@ def homepage(request):
         'homepage_text_content' : 'Text on the right',
     }
     '''
-    template = loader.get_template('djapp1/homepage.html')
-    context = {
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM dinosaur", [])
+        columns = [col[0] for col in cursor.description]
+        dinosaurs =[
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
+
+        cursor.execute("SELECT * FROM visitor", [])
+        columns = [col[0] for col in cursor.description]
+        visitors = [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
+
+        cursor.execute("SELECT * FROM employee", [])
+        columns = [col[0] for col in cursor.description]
+        employees = [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
+
+        cursor.execute("SELECT * FROM exhibit", [])
+        columns = [col[0] for col in cursor.description]
+        exhibits = [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]        
+
+        cursor.execute("SELECT * FROM zone", [])
+        columns = [col[0] for col in cursor.description]
+        zones = [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
+
+        cursor.execute("SELECT * FROM computerSystem", [])
+        columns = [col[0] for col in cursor.description]
+        computerSystems = [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]        
+    print(visitors)
+    print(dinosaurs);
+    template = loader.get_template('djapp1/index.html')
+    context = {'dinosaur_list': dinosaurs, 'visitor_list': visitors, 'employee_list': employees,
+    'exhibit_list': exhibits, 'zone_list': zones, 'computerSystem_list': computerSystems
+
     }
+
     return HttpResponse(template.render(context, request))
